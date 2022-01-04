6aaa32378790c32be71b5bf6be8a49ed5c6cc287
ZhuC <825692284@qq.com>
2022-01-04 16:28:15 +0800

    1

--- blog-theme/src/utils/hooks.ts
+++ blog-theme/src/utils/hooks.ts
@@ -60,7 +60,7 @@ export const useGetList = () => {
     data.skip = init ? 0 : data.skip + 30;
     const queryParams: GetExhibitListByPagingParams = {
       skip: data.skip,
-      articleResourceTypes: "markdown",
+      articleResourceTypes: "markdown,novel",
       limit: params.limit || 30,
       ...params,
     };
