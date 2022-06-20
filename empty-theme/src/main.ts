e5f92256209892a64adbad1113e15519a7423ede
ZhuC <825692284@qq.com>
2022-06-20 14:44:22 +0800

    完成占位主题

---
+++ empty-theme/src/main.ts
@@ -0,0 +1,54 @@
+/* eslint-disable */
+import "./public-path";
+import { createApp } from "vue";
+import App from "./App.vue";;
+
+let myWindow: any = window;
+
+let instance: any = null;
+
+function render(props: any = {}) {
+  const { container } = props;
+  instance = createApp(App)
+  instance.mount(container ? container.querySelector("#app") : "#app");
+}
+
+if (!myWindow.__POWERED_BY_FREELOG__) {
+  render();
+}
+
+export async function bootstrap() {
+  console.log("%c ", "color: green;", "vue3.0 app bootstraped");
+}
+
+export async function mount(props: { onGlobalStateChange: any; setGlobalState: any }) {
+  storeTest(props);
+  render(props);
+  if (instance.config) {
+    instance.config.globalProperties.$onGlobalStateChange = props.onGlobalStateChange;
+    instance.config.globalProperties.$setGlobalState = props.setGlobalState;
+  }
+}
+
+export async function unmount() {
+  instance.unmount();
+  instance._container.innerHTML = "";
+  instance = null;
+}
+
+// 插件通信功能暂未测试
+function storeTest(props: { onGlobalStateChange: any; setGlobalState: any; name?: any }) {
+  props.onGlobalStateChange &&
+    props.onGlobalStateChange(
+      (value: any, prev: any) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev),
+      true
+    );
+  props.setGlobalState &&
+    props.setGlobalState({
+      ignore: props.name,
+      user: {
+        name: props.name,
+      },
+    });
+}
+/* eslint-disable */
