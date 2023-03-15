55fa62683d086546aa1ce99bf2c2a665f53e9aa9
snnaenu <455342265@qq.com>
2023-03-15 15:52:14 +0800

    examples init

---
+++ guide-examples/vue3-theme/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <button @click="reload">reload</button>
+  <button @click="show">show window</button>
+  <HelloWorld msg="Vue.js Demo" @close="close"> </HelloWorld>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  created() {
+    console.log('vue2.0写法 created');
+  },
+  mounted() {
+    console.log(this.$route);
+  },
+  methods: {
+    reload() {
+      window.data = '23223232323'
+      window.location.reload()
+    },
+    show(){
+      console.log(window.data)
+    },
+    
+    close() {
+      console.log('close');
+    },
+  },
+};
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
