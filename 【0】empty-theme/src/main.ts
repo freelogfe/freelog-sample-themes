f81b345f729ad9c2162c0e96acfc15523ad1c52e
ZhuC <825692284@qq.com>
2024-05-20 17:09:45 +0800

    占位主题更新新版运行时

--- 【0】empty-theme/src/main.ts
+++ 【0】empty-theme/src/main.ts
@@ -3,33 +3,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 const myWindow: any = window;
-let instance: any = null;
+let app: any = null;
 
-function render(props: any = {}) {
-  const { container } = props;
-  instance = createApp(App);
-  instance.mount(container ? container.querySelector("#app") : "#app");
-}
+myWindow.mount = () => {
+  app = createApp(App);
+  app.mount("#app");
+};
 
-if (!myWindow.__POWERED_BY_FREELOG__) {
-  render();
-}
+myWindow.unmount = () => {
+  app.unmount();
+  app._container.innerHTML = "";
+  app = null;
+};
 
-export async function bootstrap() {
-  console.log("%c ", "color: green;", "vue3.0 app bootstraped");
-}
-
-export async function mount(props: any) {
-  render(props);
-  if (instance.config) {
-    const { onGlobalStateChange, setGlobalState } = props;
-    instance.config.globalProperties.$onGlobalStateChange = onGlobalStateChange;
-    instance.config.globalProperties.$setGlobalState = setGlobalState;
-  }
-}
-
-export async function unmount() {
-  instance.unmount();
-  instance._container.innerHTML = "";
-  instance = null;
+if (!myWindow.__MICRO_APP_ENVIRONMENT__) {
+  myWindow.mount();
 }
