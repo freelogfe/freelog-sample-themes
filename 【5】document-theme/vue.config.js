97bfbca161969eefdb33183b3f7e24ab7597ee8d
dev-tokyo <2524480125@qq.com>
2024-06-19 15:25:43 +0800

    Fix: 调整有关svg的webpack loader

--- 【5】document-theme/vue.config.js
+++ 【5】document-theme/vue.config.js
@@ -40,4 +40,16 @@ module.exports = {
       globalObject: "window",
     },
   },
+  chainWebpack: config => {
+    // 添加新的 svg 规则
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-loader-v16')
+      .loader('vue-loader-v16') // or `vue-loader-v16` if you are using a preview support of Vue 3 in Vue CLI
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  }
 };
