cd89f8cfd79272ad0dbac19737d66a1b307f5974
dev-tokyo <2524480125@qq.com>
2024-07-10 10:33:37 +0800

    Feature: 增加pinia

--- 【7】music-theme/src/main.ts
+++ 【7】music-theme/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
@@ -6,7 +7,9 @@ window.mount = () => {
   const isMicroApp = !window.__MICRO_APP_ENVIRONMENT__;
   console.log(isMicroApp ? "%c独立模块" : "%c子应用", "color:red; padding:10px; font-size: 15px");
 
+  const pinia = createPinia();
   const app = createApp(App);
+  app.use(pinia);
   app.use(router);
   app.mount("#app");
 };
--- 【7】music-theme/src/views/home/index.vue
+++ 【7】music-theme/src/views/home/index.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useGlobalStore } from "@/store/global";
+
+const store = useGlobalStore();
+const { increment } = store;
+
+const handleClick = () => {
+  increment();
+};
+</script>
 
 <template>
-  <div>主页</div>
+  <div>主页{{ store.doubleCount }}</div>
+  <button @click="handleClick">增加</button>
 </template>
