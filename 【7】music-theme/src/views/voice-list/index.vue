fa195db444734adf0bc3acaf20dc30f46fdeefaa
dev-tokyo <2524480125@qq.com>
2024-08-19 14:19:47 +0800

    Fix: FT-458 音乐主题，进入音乐页面应从顶部开始

--- 【7】music-theme/src/views/voice-list/index.vue
+++ 【7】music-theme/src/views/voice-list/index.vue
@@ -55,16 +55,6 @@ export default {
     this.getList(true);
   },
 
-  activated() {
-    const app = document.getElementById("app");
-    app.addEventListener("scroll", this.scroll);
-  },
-
-  deactivated() {
-    const app = document.getElementById("app");
-    app.removeEventListener("scroll", this.scroll);
-  },
-
   methods: {
     /** 获取音乐列表 */
     async getList(init = false) {
@@ -167,18 +157,6 @@ export default {
         this.subSkip = 0;
         this.subTempData = [];
       }
-    },
-
-    /** 页面滚动 */
-    scroll() {
-      const app = document.getElementById("app");
-      const scrollTop = app.scrollTop || 0;
-      sessionStorage.setItem("musicListScroll", scrollTop);
-      const clientHeight = app.clientHeight || 0;
-      const scrollHeight = app.scrollHeight || 0;
-      if (scrollTop + clientHeight < scrollHeight - 200) return;
-
-      this.getList();
     }
   }
 };
