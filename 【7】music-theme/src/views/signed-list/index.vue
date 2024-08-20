a2768218ec01baee95e918aa1daa93fa9aa2638e
dev-tokyo <2524480125@qq.com>
2024-08-20 11:22:01 +0800

    Fix: FT-442 音乐主题 签约记录页也应显示专辑里的单品

--- 【7】music-theme/src/views/signed-list/index.vue
+++ 【7】music-theme/src/views/signed-list/index.vue
@@ -1,9 +1,86 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from "vue";
+import { freelogApp } from "freelog-runtime";
+import list from "@/components/list.vue";
+import { useGlobalStore } from "@/store/global";
+
+const store = useGlobalStore();
+const collectionData = ref([]);
+const signedData = ref(store.signedList);
+
+const getList = async () => {
+  for (const item of store.signedList) {
+    if (item.articleInfo.articleType === 2) {
+      await getCollectionList(item.exhibitId, item.exhibitName, item.coverImages);
+    }
+  }
+};
+
+/** 获取合集里的单品列表 */
+let subTotal = 0;
+let subSkip = 0;
+let subTempData = [];
+
+const getCollectionList = async (collectionID: string, exhibitName: string, images: string[]) => {
+  const subList = await freelogApp.getCollectionSubList(collectionID, {
+    skip: subSkip,
+    limit: 1_000,
+    isShowDetailInfo: 1
+  });
+  const { dataList, totalItem } = subList.data.data;
+  subTotal = totalItem;
+
+  if (dataList.length !== 0) {
+    const ids = dataList.map((item: any) => item.itemId).join();
+    const statusInfo = await (freelogApp as any).getCollectionSubAuth(collectionID, {
+      itemIds: ids
+    });
+
+    if (statusInfo.data.data) {
+      dataList.forEach((item: Exhibit) => {
+        const index = statusInfo.data.data.findIndex(
+          resultItem => resultItem.itemId === item.itemId
+        );
+        if (index !== -1) {
+          item.defaulterIdentityType = statusInfo.data.data[index].defaulterIdentityType;
+        }
+
+        item.updateDate = item.articleInfo.latestVersionReleaseDate;
+        item.coverImages = images;
+        item.versionInfo = { exhibitProperty: item.articleInfo?.articleProperty };
+        item.exhibitTitle = item.itemTitle;
+        item.exhibitIntro = item.articleInfo.intro;
+        item.albumName = exhibitName;
+        item.exhibitId = collectionID;
+      });
+    }
+  }
+
+  subTempData.push(...dataList);
+  collectionData.value = [...collectionData.value, ...dataList];
+  signedData.value = [...signedData.value, ...collectionData.value];
+
+  if (subTempData.length < subTotal) {
+    subSkip = subSkip + 1_000;
+    await getCollectionList(collectionID, exhibitName, images);
+  } else {
+    subTotal = 0;
+    subSkip = 0;
+    subTempData = [];
+  }
+};
+
+onBeforeMount(() => {
+  getList();
+});
+</script>
+
 <!-- 签约列表页 -->
 <template>
   <div class="sign-list-wrapper">
     <list
-      :list="store.signedList"
-      :loading="!store.signedList"
+      :list="signedData"
+      :loading="!signedData"
       title="签约记录"
       noDataTip="暂无任何签约记录"
       statusShow
@@ -11,22 +88,3 @@
     />
   </div>
 </template>
-
-<script>
-import list from "@/components/list.vue";
-import { useGlobalStore } from "@/store/global";
-
-export default {
-  name: "sign-list",
-
-  components: { list },
-
-  data() {
-    const store = useGlobalStore();
-
-    return {
-      store
-    };
-  }
-};
-</script>
