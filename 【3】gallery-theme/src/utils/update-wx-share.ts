6932863ea0c92e608a3cfe4e4dcefe8ac0dc63dd
dev-tokyo <2524480125@qq.com>
2025-07-25 16:22:09 +0800

    Fix: eslint

--- 【3】gallery-theme/src/utils/update-wx-share.ts
+++ 【3】gallery-theme/src/utils/update-wx-share.ts
@@ -1,15 +1,15 @@
 import { freelogApp } from "freelog-runtime";
 
 export const updateWxConfig = (exhibitInfo: {
-  exhibitIntro: string,
-  coverImages: Array<string>,
-  exhibitTitle: string
+  exhibitIntro: string;
+  coverImages: Array<string>;
+  exhibitTitle: string;
 }) => {
-  const { exhibitIntro, coverImages, exhibitTitle } = exhibitInfo
-  const isWeChatOrQQ = (freelogApp as any).isWeChatOrQQ()
+  const { exhibitIntro, coverImages, exhibitTitle } = exhibitInfo;
+  const isWeChatOrQQ = (freelogApp as any).isWeChatOrQQ();
   if (isWeChatOrQQ) {
-    const defaultImg = "//static.testfreelog.com/static/default_cover.png"
-    let imgUrl = coverImages[0] === defaultImg ? "" : coverImages[0]
+    const defaultImg = "//static.testfreelog.com/static/default_cover.png";
+    const imgUrl = coverImages[0] === defaultImg ? "" : coverImages[0];
     const fShare = {
       title: exhibitTitle,
       desc: exhibitIntro,
@@ -19,6 +19,6 @@ export const updateWxConfig = (exhibitInfo: {
       title: exhibitTitle,
       imgUrl
     };
-    (freelogApp as any).updateWechatShare(fShare, qShare)
+    (freelogApp as any).updateWechatShare(fShare, qShare);
   }
-}
+};
