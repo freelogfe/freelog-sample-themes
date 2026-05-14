5826fe6cd29f0615997afc04bb8153d8a2a7276d
dev-tokyo <2524480125@qq.com>
2026-05-14 16:39:54 +0800

    Fix: ZhuC/Freelog插件-主页分享

--- 【7】music-theme/src/App.vue
+++ 【7】music-theme/src/App.vue
@@ -36,7 +36,9 @@ const mountShareWidget = async () => {
 
   const subDeps = await freelogApp.getSelfDepForTheme();
   const widgetData = subDeps.find(
-    (item: { articleName?: string }) => item.articleName === "ZhuC/_Freelog插件-主页分享"
+    (item: { articleName?: string }) =>
+      item.articleName === "ZhuC/_Freelog插件-主页分享" ||
+      item.articleName === "ZhuC/Freelog插件-主页分享"
   );
   if (!widgetData) return;
 
